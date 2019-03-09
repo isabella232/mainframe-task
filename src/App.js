7bdf163ef4d190993694e5cec9f3705e1ad38310
Guilherme Viana <ggviana@hotmail.com.br>
2019-03-09 18:19:35 -0300

    Added global styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { backgroundWhite, textPrimaryColor } from 'theme'
+import { LinkButton } from 'components'
 
-class App extends Component {
+const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    background-color: ${backgroundWhite};
+    color: ${textPrimaryColor};
+  }
+`
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        <GlobalStyles />
+        <LinkButton href='/#get-started'>
+          Get started
+        </LinkButton>
+      </>
+    )
   }
 }
 
