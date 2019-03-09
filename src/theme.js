2e56d60eb891724a2499943a530237976c917fb4
Guilherme Viana <ggviana@hotmail.com.br>
2019-03-09 18:58:46 -0300

    Added media queries

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { css } from 'styled-components'
+
 export const primaryColor = '#142543'
 export const successColor = '#00a7e7'
 
@@ -6,3 +8,20 @@ export const backgroundGrey = '#f7f7f7'
 
 export const textPrimaryColor = '#232323'
 export const textSecondaryColor = '#8992a1'
+
+const sizes = {
+  xs: 576,
+  sm: 768,
+  md: 992,
+  lg: 1200
+}
+
+export const media = Object.keys(sizes).reduce((acc, size) => {
+  acc[size] = (...args) => css`
+    @media (min-width: ${sizes[size]}px) {
+      ${css(...args)}
+    }
+  `
+
+  return acc
+}, {})
