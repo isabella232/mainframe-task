1539ec943854e88e0d37a6fcb799747b39ed41de
Guilherme Viana <ggviana@hotmail.com.br>
2019-03-09 17:24:37 -0300

    Added theme colors

---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#142543'
+export const successColor = '#00a7e7'
+
+export const backgroundWhite = '#fff'
+export const backgroundGrey = '#f7f7f7'
+
+export const textPrimaryColor = '#232323'
+export const textSecondaryColor = '#8992a1'
