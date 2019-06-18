79d2bcf0563dbf42014c389b19acae92710e6c37
Guilherme Viana <ggviana@hotmail.com.br>
2019-06-18 20:00:39 -0300

    Updates

---
+++ precache-manifest.6e99e2a9a47a14152143ced072d2027e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb24ad27c6a851278e8a8b998830ddc5",
+    "url": "/mainframe-task/index.html"
+  },
+  {
+    "revision": "daed50fbb8bf02848833",
+    "url": "/mainframe-task/static/css/main.3192d064.chunk.css"
+  },
+  {
+    "revision": "3768cf6dceb37166b3a0",
+    "url": "/mainframe-task/static/js/2.3e7066cc.chunk.js"
+  },
+  {
+    "revision": "daed50fbb8bf02848833",
+    "url": "/mainframe-task/static/js/main.1b4d8130.chunk.js"
+  },
+  {
+    "revision": "ebf0471f27618732f842",
+    "url": "/mainframe-task/static/js/runtime~main.61a57281.js"
+  },
+  {
+    "revision": "eeeb594ecbb2e106230b8d66deec9a02",
+    "url": "/mainframe-task/static/media/mainframe-logo.eeeb594e.png"
+  }
+]);
